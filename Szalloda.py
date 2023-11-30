0d183435045a69217c22b43133893a4977fd9db9
Zsolt Neszter <zneszter@gmail.com>
2023-11-30 17:01:56 +0100

    foglalas  és lemondas metodus hozzáadása

--- Szalloda.py
+++ Szalloda.py
@@ -2,3 +2,17 @@ class Szalloda:
     def __init__(self, nev):
         self.nev = nev
         self.szobak = []
+        self.foglalasok = []
+
+
+    def foglalas(self, szobaszam, datum):
+        szoba = next((s for s in self.szobak if s.szobaszam == szobaszam), None)
+        if szoba and not self._foglalt(szoba, datum):
+            foglalas = Foglalas(szoba, datum)
+            self.foglalasok.append(foglalas)
+            return foglalas
+        return None
+
+    def lemondas(self, foglalas):
+        if foglalas in self.foglalasok:
+            self.foglalasok.remove(foglalas)
