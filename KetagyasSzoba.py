70f6fdd2c6fa665cbb508f006cc3c8f2c0d482c4
Zsolt Neszter <zneszter@gmail.com>
2023-11-30 16:47:31 +0100

    egyagyas es ketagyas szoba orokitese a szoba osztályból

---
+++ KetagyasSzoba.py
@@ -0,0 +1,7 @@
+class KetagyasSzoba(Szoba):
+    def __init__(self, szobaszam, ar=20000, egyedi_attributum):
+        super().__init__(szobaszam, ar)
+        self.egyedi_attributum = egyedi_attributum
+
+    def __str__(self):
+        return f"Kétágyas szoba {self.szobaszam} - {self.ar} Ft/éjszaka, Egyedi attribútum: {self.egyedi_attributum}"
